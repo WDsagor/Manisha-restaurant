4541f9066fcd6bd1f0b6115b0bbf335ad8b71e48
Wdsagor <sagarwpress@gmail.com>
2024-06-17 14:05:34 +0600

    update menu item design

--- app/restaurant/ItemsCard.js
+++ app/restaurant/ItemsCard.js
@@ -1,35 +1,39 @@
-"use client"
+"use client";
 import SingleListCard from "./SingleListCard";
 import foodMenu from "./FoodMenu";
 import { useState } from "react";
 
 const ItemsCard = () => {
-  const [foodItems, setFoodItems]= useState(foodMenu)
-  const handleCatagoriesItems=(id)=>{
-    const foodCatagories=foodMenu.filter(fooItems => fooItems.id===id)
+  const [foodItems, setFoodItems] = useState(foodMenu);
+  const handleCatagoriesItems = (id) => {
+    const foodCatagories = foodMenu.filter((fooItems) => fooItems.id === id);
     setFoodItems(foodCatagories);
-    
-  }
+  };
 
   return (
     <div className="max-w-screen-2xl mx-auto">
       <div className=" text-center py-10">
         <h1 className="text-5xl text-primary font-bold">Our Menu</h1>
       </div>
-      <div className="grid grid-cols-2 md:grid-cols-4 xl:grid-cols-5 gap-2 max-w-screen-md mx-auto">
-        <button onClick={()=>setFoodItems(foodMenu)} className="btn btn-outline  btn-primary">All Menus</button>
+      <div className="grid px-2 grid-cols-2 md:grid-cols-4 xl:grid-cols-5 gap-2 max-w-screen-md mx-auto">
+        <button
+          onClick={() => setFoodItems(foodMenu)}
+          className="btn btn-outline  uppercase btn-primary"
+        >
+          All Menus
+        </button>
         {foodMenu.map((fooditem) => {
-        
           return (
             <>
-            <button onClick={()=>handleCatagoriesItems(fooditem?.id)}
-              className="btn btn-outline  btn-primary"
-              key={fooditem?.id}
-              fooditem={fooditem}
+              <button
+                onClick={() => handleCatagoriesItems(fooditem?.id)}
+                className="btn btn-outline uppercase  btn-primary"
+                key={fooditem?.id}
+                fooditem={fooditem}
               >
-              {fooditem?.catagoryName}
-            </button>
-              </>
+                {fooditem?.catagoryName}
+              </button>
+            </>
           );
         })}
       </div>
--- app/restaurant/SingleListCard.js
+++ app/restaurant/SingleListCard.js
@@ -1,33 +1,51 @@
+import Image from "next/image";
+
 const SingleListCard = ({ fooditem }) => {
   const { catagoryName, items, discrip } = fooditem;
   return (
-    <div className=" max-w-screen-xl mx-auto pb-10">
+    <div className=" max-w-screen-2xl mx-auto pb-10">
       <div className=" max-w-screen-md mx-auto py-5">
         <h1 className="text-3xl text-center text-primary py-5">
           {catagoryName}
         </h1>
         {discrip && <p className="text-center">{discrip}</p>}
       </div>
-      <div className="grid grid-cols-2 gap-x-7 gap-5 max-w-screen-lg mx-auto px-3">
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-x-7 gap-5 max-w-screen-2xl mx-auto px-3">
         {items.map((item) => {
           return (
             <div
               key={item?.id}
-              className=" hover:scale-105 transition-all cursor-pointer flex justify-between gap-2 hover:shadow-lg p-3 rounded-lg"
+              className=" hover:scale-105 transition-all cursor-pointer flex justify-between space-x-3 gap-2 hover:shadow-lg p-3 rounded-lg"
             >
-              <div>
-                <h1 className=" font-bold text-primary text-lg">
-                  {item?.name}
-                </h1>
-                <p className=" font-normal">{item?.discription}</p>
-                {item?.lamb && (
-                  <p className=" font-normal mt-5">Lamb : {item?.lamb}</p>
-                )}
-                {item?.kingPrawns && (
-                  <p className=" font-normal">King Prawns{item?.kingPrawns}</p>
-                )}
+              <div className="flex space-x-4">
+                <Image
+                  className="rounded-ee-[60px] rounded-es-[70px]	rounded-se-[50px]"
+                  src={"/images/Chicken-Tikka-Biryani.jpg"}
+                  height={100}
+                  width={150}
+                />
+
+                <div>
+                  <h1 className=" font-bold text-primary text-lg">
+                    {item?.name}
+                  </h1>
+                  <p className=" font-normal">{item?.discription}</p>
+                  {item?.lamb && (
+                    <p className=" font-normal mt-5">Lamb : {item?.lamb}</p>
+                  )}
+                  {item?.kingPrawns && (
+                    <p className=" font-normal">
+                      King Prawns{item?.kingPrawns}
+                    </p>
+                  )}
+                </div>
               </div>
-              <p className="text-lg font-bold text-primary">£{item?.price}</p>
+              <div className="shrink-0">
+                <p className="text-lg font-bold text-primary">£{item?.price}</p>
+                <button className="btn btn-sm mt-5 uppercase ">
+                  Add to cart
+                </button>
+              </div>
             </div>
           );
         })}
